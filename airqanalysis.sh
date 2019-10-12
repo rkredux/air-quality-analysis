b12d5039732f1f8c8e10e64f2b294d8dc78055e9
rkumar <rkredux@gmail.com>
2019-10-12 01:10:57 -0400

    Adding Aggregation

--- airqanalysis.sh
+++ airqanalysis.sh
@@ -15,7 +15,7 @@ awk -F, '$2== "US" { a[$1] } END { for (i in a) print i}' airquality.csv >> list
 
 
 #Top 20 readings for a particular parameter 
-time awk -F, '$7 == "co" { print $1,$2,$5,$7,$8}' OFS=, airquality.csv | parallel --pipe --block 10M  sort -t ',' -k5 -n -r | head -20 >> top20_co_reading.txt
+time awk -F, '$7 == "co" { print $1,$2,$5,$7,$8}' OFS=, airquality.csv | sort -t ',' -k5 -n -r | head -20 >> top20_co_reading.txt
 
 
 #running calculations 
@@ -31,6 +31,15 @@ time awk -F, '$2 == "US" && $5 ~ "2018-01" && $7 == "co"{ $5=substr($5,1,10); pr
 #US,2018-01,co,0.44
 #US,2018-02,co,0.42
 awk -F, '$2 == "US" && $5 ~ "2018" && $7 == "co"{ $5=substr($5,1,7); print $2,$5,$7,$8}' OFS=, airquality.csv | awk -F, '{a[$2] += $4} END {for (i in a)print i,a[i]/NR}' 
+
+
+
+#Avg value for each month per parameter 
+#similar to SQL nested subqueries (Loop within a loop)
+#US,2018-01,co,0.44
+#US,2018-01,so2,0.34
+# awk -F, '$2 == "US" && $5 ~ "2018"{ $5=substr($5,1,7); print $2,$5,$7,$8}' OFS=, airquality.csv | awk -F, '{b[$5]} {a[$3] += $4} END {for (i in b)print i,b[i],a/NR}' 
+
 
 
 #sources to read more on subject 
